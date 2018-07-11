f8eb6b832f13460d13e53cc3033eaa698c1c9bfb
Arseny <arsenyparamonov@gmail.com>
2018-07-11 19:00:28 +0300

    Add photos, fix paddings

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,6 +21,16 @@ class AboutPage extends React.Component {
 const IMAGES =
     [
         {
+            src: "/static/1d.jpg",
+            thumbnail: "/static/1m.jpg",
+            srcSet: [
+                '/static/1m.jpg 320w',
+                '/static/1t.jpg 640w',
+                '/static/1d.jpg 960w'
+            ],
+            thumbnailWidth: 240,
+        },
+        {
             src: "/static/2d.jpg",
             thumbnail: "/static/2m.jpg",
             srcSet: [
@@ -101,12 +111,172 @@ const IMAGES =
             thumbnailWidth: 240,
         },
         {
-            src: "/static/8d.jpg",
-            thumbnail: "/static/8m.jpg",
+            src: "/static/10d.jpg",
+            thumbnail: "/static/10m.jpg",
             srcSet: [
-                '/static/8m.jpg 320w',
-                '/static/8t.jpg 640w',
-                '/static/8d.jpg 960w'
+                '/static/10m.jpg 320w',
+                '/static/10t.jpg 640w',
+                '/static/10d.jpg 960w'
+            ],
+            thumbnailWidth: 240,
+        },
+        {
+            src: "/static/11d.jpg",
+            thumbnail: "/static/11m.jpg",
+            srcSet: [
+                '/static/11m.jpg 320w',
+                '/static/11t.jpg 640w',
+                '/static/11d.jpg 960w'
+            ],
+            thumbnailWidth: 240,
+        },
+        {
+            src: "/static/12d.jpg",
+            thumbnail: "/static/12m.jpg",
+            srcSet: [
+                '/static/12m.jpg 320w',
+                '/static/12t.jpg 640w',
+                '/static/12d.jpg 960w'
+            ],
+            thumbnailWidth: 240,
+        },
+        {
+            src: "/static/13d.jpg",
+            thumbnail: "/static/13m.jpg",
+            srcSet: [
+                '/static/13m.jpg 320w',
+                '/static/13t.jpg 640w',
+                '/static/13d.jpg 960w'
+            ],
+            thumbnailWidth: 240,
+        },
+        {
+            src: "/static/14d.jpg",
+            thumbnail: "/static/14m.jpg",
+            srcSet: [
+                '/static/14m.jpg 320w',
+                '/static/14t.jpg 640w',
+                '/static/14d.jpg 960w'
+            ],
+            thumbnailWidth: 240,
+        },
+        {
+            src: "/static/15d.jpg",
+            thumbnail: "/static/15m.jpg",
+            srcSet: [
+                '/static/15m.jpg 320w',
+                '/static/15t.jpg 640w',
+                '/static/15d.jpg 960w'
+            ],
+            thumbnailWidth: 240,
+        },
+        {
+            src: "/static/16d.jpg",
+            thumbnail: "/static/16m.jpg",
+            srcSet: [
+                '/static/16m.jpg 320w',
+                '/static/16t.jpg 640w',
+                '/static/16d.jpg 960w'
+            ],
+            thumbnailWidth: 240,
+        },
+        {
+            src: "/static/17d.jpg",
+            thumbnail: "/static/17m.jpg",
+            srcSet: [
+                '/static/17m.jpg 320w',
+                '/static/17t.jpg 640w',
+                '/static/17d.jpg 960w'
+            ],
+            thumbnailWidth: 240,
+        },
+        {
+            src: "/static/18d.jpg",
+            thumbnail: "/static/18m.jpg",
+            srcSet: [
+                '/static/18m.jpg 320w',
+                '/static/18t.jpg 640w',
+                '/static/18d.jpg 960w'
+            ],
+            thumbnailWidth: 240,
+        },
+        {
+            src: "/static/19d.jpg",
+            thumbnail: "/static/19m.jpg",
+            srcSet: [
+                '/static/19m.jpg 320w',
+                '/static/19t.jpg 640w',
+                '/static/19d.jpg 960w'
+            ],
+            thumbnailWidth: 240,
+        },
+        {
+            src: "/static/20d.jpg",
+            thumbnail: "/static/21m.jpg",
+            srcSet: [
+                '/static/21m.jpg 320w',
+                '/static/21t.jpg 640w',
+                '/static/21d.jpg 960w'
+            ],
+            thumbnailWidth: 240,
+        },
+        {
+            src: "/static/22d.jpg",
+            thumbnail: "/static/22m.jpg",
+            srcSet: [
+                '/static/22m.jpg 320w',
+                '/static/22t.jpg 640w',
+                '/static/22d.jpg 960w'
+            ],
+            thumbnailWidth: 240,
+        },
+        {
+            src: "/static/23d.jpg",
+            thumbnail: "/static/23m.jpg",
+            srcSet: [
+                '/static/23m.jpg 320w',
+                '/static/23t.jpg 640w',
+                '/static/23d.jpg 960w'
+            ],
+            thumbnailWidth: 240,
+        },
+        {
+            src: "/static/24d.jpg",
+            thumbnail: "/static/24m.jpg",
+            srcSet: [
+                '/static/24m.jpg 320w',
+                '/static/24t.jpg 640w',
+                '/static/24d.jpg 960w'
+            ],
+            thumbnailWidth: 240,
+        },
+        {
+            src: "/static/25d.jpg",
+            thumbnail: "/static/25m.jpg",
+            srcSet: [
+                '/static/25m.jpg 320w',
+                '/static/25t.jpg 640w',
+                '/static/25d.jpg 960w'
+            ],
+            thumbnailWidth: 240,
+        },
+        {
+            src: "/static/26d.jpg",
+            thumbnail: "/static/26m.jpg",
+            srcSet: [
+                '/static/26m.jpg 320w',
+                '/static/26t.jpg 640w',
+                '/static/26d.jpg 960w'
+            ],
+            thumbnailWidth: 240,
+        },
+        {
+            src: "/static/27d.jpg",
+            thumbnail: "/static/27m.jpg",
+            srcSet: [
+                '/static/27m.jpg 320w',
+                '/static/27t.jpg 640w',
+                '/static/27d.jpg 960w'
             ],
             thumbnailWidth: 240,
         },
@@ -114,6 +284,16 @@ const IMAGES =
 
 const IMAGESMOBILE =
     [
+        {
+            src: "/static/1d.jpg",
+            thumbnail: "/static/1m.jpg",
+            srcSet: [
+                '/static/1m.jpg 320w',
+                '/static/1t.jpg 640w',
+                '/static/1d.jpg 960w'
+            ],
+            thumbnailWidth: 240,
+        },
         {
             src: "/static/2d.jpg",
             thumbnail: "/static/2m.jpg",
@@ -195,12 +375,172 @@ const IMAGESMOBILE =
             thumbnailWidth: 240,
         },
         {
-            src: "/static/8d.jpg",
-            thumbnail: "/static/8m.jpg",
+            src: "/static/10d.jpg",
+            thumbnail: "/static/10m.jpg",
             srcSet: [
-                '/static/8m.jpg 320w',
-                '/static/8t.jpg 640w',
-                '/static/8d.jpg 960w'
+                '/static/10m.jpg 320w',
+                '/static/10t.jpg 640w',
+                '/static/10d.jpg 960w'
+            ],
+            thumbnailWidth: 240,
+        },
+        {
+            src: "/static/11d.jpg",
+            thumbnail: "/static/11m.jpg",
+            srcSet: [
+                '/static/11m.jpg 320w',
+                '/static/11t.jpg 640w',
+                '/static/11d.jpg 960w'
+            ],
+            thumbnailWidth: 240,
+        },
+        {
+            src: "/static/12d.jpg",
+            thumbnail: "/static/12m.jpg",
+            srcSet: [
+                '/static/12m.jpg 320w',
+                '/static/12t.jpg 640w',
+                '/static/12d.jpg 960w'
+            ],
+            thumbnailWidth: 240,
+        },
+        {
+            src: "/static/13d.jpg",
+            thumbnail: "/static/13m.jpg",
+            srcSet: [
+                '/static/13m.jpg 320w',
+                '/static/13t.jpg 640w',
+                '/static/13d.jpg 960w'
+            ],
+            thumbnailWidth: 240,
+        },
+        {
+            src: "/static/14d.jpg",
+            thumbnail: "/static/14m.jpg",
+            srcSet: [
+                '/static/14m.jpg 320w',
+                '/static/14t.jpg 640w',
+                '/static/14d.jpg 960w'
+            ],
+            thumbnailWidth: 240,
+        },
+        {
+            src: "/static/15d.jpg",
+            thumbnail: "/static/15m.jpg",
+            srcSet: [
+                '/static/15m.jpg 320w',
+                '/static/15t.jpg 640w',
+                '/static/15d.jpg 960w'
+            ],
+            thumbnailWidth: 240,
+        },
+        {
+            src: "/static/16d.jpg",
+            thumbnail: "/static/16m.jpg",
+            srcSet: [
+                '/static/16m.jpg 320w',
+                '/static/16t.jpg 640w',
+                '/static/16d.jpg 960w'
+            ],
+            thumbnailWidth: 240,
+        },
+        {
+            src: "/static/17d.jpg",
+            thumbnail: "/static/17m.jpg",
+            srcSet: [
+                '/static/17m.jpg 320w',
+                '/static/17t.jpg 640w',
+                '/static/17d.jpg 960w'
+            ],
+            thumbnailWidth: 240,
+        },
+        {
+            src: "/static/18d.jpg",
+            thumbnail: "/static/18m.jpg",
+            srcSet: [
+                '/static/18m.jpg 320w',
+                '/static/18t.jpg 640w',
+                '/static/18d.jpg 960w'
+            ],
+            thumbnailWidth: 240,
+        },
+        {
+            src: "/static/19d.jpg",
+            thumbnail: "/static/19m.jpg",
+            srcSet: [
+                '/static/19m.jpg 320w',
+                '/static/19t.jpg 640w',
+                '/static/19d.jpg 960w'
+            ],
+            thumbnailWidth: 240,
+        },
+        {
+            src: "/static/20d.jpg",
+            thumbnail: "/static/21m.jpg",
+            srcSet: [
+                '/static/21m.jpg 320w',
+                '/static/21t.jpg 640w',
+                '/static/21d.jpg 960w'
+            ],
+            thumbnailWidth: 240,
+        },
+        {
+            src: "/static/22d.jpg",
+            thumbnail: "/static/22m.jpg",
+            srcSet: [
+                '/static/22m.jpg 320w',
+                '/static/22t.jpg 640w',
+                '/static/22d.jpg 960w'
+            ],
+            thumbnailWidth: 240,
+        },
+        {
+            src: "/static/23d.jpg",
+            thumbnail: "/static/23m.jpg",
+            srcSet: [
+                '/static/23m.jpg 320w',
+                '/static/23t.jpg 640w',
+                '/static/23d.jpg 960w'
+            ],
+            thumbnailWidth: 240,
+        },
+        {
+            src: "/static/24d.jpg",
+            thumbnail: "/static/24m.jpg",
+            srcSet: [
+                '/static/24m.jpg 320w',
+                '/static/24t.jpg 640w',
+                '/static/24d.jpg 960w'
+            ],
+            thumbnailWidth: 240,
+        },
+        {
+            src: "/static/25d.jpg",
+            thumbnail: "/static/25m.jpg",
+            srcSet: [
+                '/static/25m.jpg 320w',
+                '/static/25t.jpg 640w',
+                '/static/25d.jpg 960w'
+            ],
+            thumbnailWidth: 240,
+        },
+        {
+            src: "/static/26d.jpg",
+            thumbnail: "/static/26m.jpg",
+            srcSet: [
+                '/static/26m.jpg 320w',
+                '/static/26t.jpg 640w',
+                '/static/26d.jpg 960w'
+            ],
+            thumbnailWidth: 240,
+        },
+        {
+            src: "/static/27d.jpg",
+            thumbnail: "/static/27m.jpg",
+            srcSet: [
+                '/static/27m.jpg 320w',
+                '/static/27t.jpg 640w',
+                '/static/27d.jpg 960w'
             ],
             thumbnailWidth: 240,
         },
