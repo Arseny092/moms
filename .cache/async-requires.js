02d6d7c4a626da5776e2b53d642144f7a01ad54b
Arseny <arsenyparamonov@gmail.com>
2018-08-27 23:28:40 +0300

    Fixed cms routes

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -3,7 +3,6 @@ const preferDefault = m => m && m.default || m
 
 exports.components = {
   "component---src-templates-blog-post-js": require("gatsby-module-loader?name=component---src-templates-blog-post-js!/Users/arseny/Documents/momsband/src/templates/blog-post.js"),
-  "component---cache-dev-404-page-js": require("gatsby-module-loader?name=component---cache-dev-404-page-js!/Users/arseny/Documents/momsband/.cache/dev-404-page.js"),
   "component---src-pages-404-js": require("gatsby-module-loader?name=component---src-pages-404-js!/Users/arseny/Documents/momsband/src/pages/404.js"),
   "component---src-pages-about-js": require("gatsby-module-loader?name=component---src-pages-about-js!/Users/arseny/Documents/momsband/src/pages/about.js"),
   "component---src-pages-blog-js": require("gatsby-module-loader?name=component---src-pages-blog-js!/Users/arseny/Documents/momsband/src/pages/blog.js"),
@@ -12,15 +11,13 @@ exports.components = {
 
 exports.json = {
   "layout-index.json": require("gatsby-module-loader?name=path---!/Users/arseny/Documents/momsband/.cache/json/layout-index.json"),
-  "hello-world-two.json": require("gatsby-module-loader?name=path---hello-world-two!/Users/arseny/Documents/momsband/.cache/json/hello-world-two.json"),
   "alla.json": require("gatsby-module-loader?name=path---alla!/Users/arseny/Documents/momsband/.cache/json/alla.json"),
-  "dev-404-page.json": require("gatsby-module-loader?name=path---dev-404-page!/Users/arseny/Documents/momsband/.cache/json/dev-404-page.json"),
+  "dasha.json": require("gatsby-module-loader?name=path---dasha!/Users/arseny/Documents/momsband/.cache/json/dasha.json"),
   "404.json": require("gatsby-module-loader?name=path---404!/Users/arseny/Documents/momsband/.cache/json/404.json"),
   "about.json": require("gatsby-module-loader?name=path---about!/Users/arseny/Documents/momsband/.cache/json/about.json"),
   "blog.json": require("gatsby-module-loader?name=path---blog!/Users/arseny/Documents/momsband/.cache/json/blog.json"),
   "index.json": require("gatsby-module-loader?name=path---index!/Users/arseny/Documents/momsband/.cache/json/index.json"),
-  "404-html.json": require("gatsby-module-loader?name=path---404-html!/Users/arseny/Documents/momsband/.cache/json/404-html.json"),
-  "dasha.json": require("gatsby-module-loader?name=path---dasha!/Users/arseny/Documents/momsband/.cache/json/dasha.json")
+  "404-html.json": require("gatsby-module-loader?name=path---404-html!/Users/arseny/Documents/momsband/.cache/json/404-html.json")
 }
 
 exports.layouts = {
